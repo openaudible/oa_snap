9a73b505ec09bea6d6b0e169a23fdf9d6b050c38
openaudible <racer@torguard.tg>
2022-02-22 09:26:27 -0800

    update launch

--- launch.sh
+++ launch.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 DIR="$(dirname "$(readlink -f "$0")")"
 cd $DIR
-./jre/bin/java -Duser.home=$SNAP_USER_COMMON -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -jar openaudible_gtk_x86_64.jar > /dev/null 2>&1 &
+UH=$SNAP_USER_COMMON
+if [ -z "$" ] ; then 
+UH=$HOME
+fi
+
+OA_MODE=snap 
+args=""
+debug=0
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -v|--version) arg="--version"; shift;;
+    -h|--help) 
+	echo "OpenAudible is a GUI Audiobook Manager. Available arguments are -v|--version to get version, and -d|--debug to run in foreground mode with stderr/stdout"
+	exit 0;
+	;;
+	-d|--debug) debug=1; shift;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+
+    esac
+done
+echo "Starting OpenAudible: debug=$debug args=$args help=$help home=$UH"
+
+
+if [ $debug -eq 0 ] ; then 
+   ./jre/bin/java -Duser.home=$UH -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -DOA_MODE=snap -jar openaudible_gtk_x86_64.jar $args > /dev/null 2>&1 &
+else
+   ./jre/bin/java -Duser.home=$UH -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -DOA_MODE=snap -jar openaudible_gtk_x86_64.jar $args 
+fi
+
 
